bf2004fbd77f25d33554d8114b75da9b06fab532
oldnews1 <235405775+oldnews1@users.noreply.github.com>
2026-03-03 18:25:44 -0700

    Implement testCountCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -37,4 +37,15 @@ public class CustomListTest {
             cityList.deleteCity(new City("Toronto", "Ontario"));
         });
     }
+
+    @Test
+    void testCountCities() {
+        CustomList cityList = mockCityList();
+        assertEquals(1, cityList.countCities());
+        City city = new City("Calgary", "Alberta");
+        cityList.addCity(city);
+        assertEquals(2, cityList.countCities());
+        cityList.deleteCity(city);
+        assertEquals(1, cityList.countCities());
+    }
 }
